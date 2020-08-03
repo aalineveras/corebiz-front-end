139e7577ea0cf0fcf61aedd00550b5f981018bf9
Aline Veras <aalineveras@gmail.com>
2020-08-03 11:42:50 -0300

    fix: shelf

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,26 @@ import ProductShelf from './components/ProductShelf'
 let elMainShelf = document.getElementById('shelfProducts');
 let elProdutShelf = document.querySelector('.shelf__list--products');
 
+let swiperConfig = {
+  slidesPerView: 4,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   fetch('https://corebiz-test.herokuapp.com/api/v1/products')
     .then(res => res.json())
     .then(res => {
       new ProductShelf(elProdutShelf, res)
-      new Swiper(elMainShelf)
+      new Swiper(elMainShelf, swiperConfig)
     })
 });
